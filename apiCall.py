4ed451b59705524281f1894338f92d052c919ad2
GokuGunZ <63858958+GokuGunZ@users.noreply.github.com>
2024-06-24 21:50:23 +0200

    Create apiCall.py

---
+++ apiCall.py
@@ -0,0 +1,14 @@
+import request
+
+def main():
+  global urlBase
+  urlBase = "https://api.spotify.com/v1/
+  global username
+  username = gokugunz
+
+def createPlaylist(name, description):
+  url = urlBase+"users/"+username+"/playlists
+
+if __name__=="main":
+  main()
+  
